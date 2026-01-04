9af91b46e43986743df6bb7efd9c99b9ff96e10b
gururaj02 <ngururaj58@gmail.com>
2026-01-04 18:59:10 +0530

    Initial Spring Boot banking application setup

---
+++ src/main/java/com/example/banking_application_spring_boot/BankingApplicationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.banking_application_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApplicationSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApplicationSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/banking_application_spring_boot/BankingApplicationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banking_application_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
