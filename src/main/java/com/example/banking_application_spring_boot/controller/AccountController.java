8d34294f3523865147e2522c9d772a164f6afa58
gururaj02 <ngururaj58@gmail.com>
2026-03-09 19:06:21 +0530

    Updated TODO's

--- src/main/java/com/example/banking_application_spring_boot/controller/AccountController.java
+++ src/main/java/com/example/banking_application_spring_boot/controller/AccountController.java
@@ -74,6 +74,8 @@ public class AccountController {
         return ResponseEntity.ok(accounts);
     }
 
+    // TODO: Save the transactions of a user in Transactions table
+
     // TODO: Get All Transactions for a particular user
 
     // TODO: Delete user Account
