ffd5c4ee40b6bfb78da54b8209186d9b9a1896ad
gururaj02 <ngururaj58@gmail.com>
2026-03-20 23:54:31 +0530

    Update AccountController.java

--- src/main/java/com/example/banking_application_spring_boot/controller/AccountController.java
+++ src/main/java/com/example/banking_application_spring_boot/controller/AccountController.java
@@ -24,6 +24,7 @@ public class AccountController {
     // TODO: Dockerize this project
     // TODO: Add tests - JUnit & Mockito
     // TODO: Add Loggers(AOP)
+    // TODO: Try to add Kafka
 
 
     // Add Account REST API
