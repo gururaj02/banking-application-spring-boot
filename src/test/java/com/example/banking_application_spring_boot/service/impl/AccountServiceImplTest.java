1852b1f8f8628fd3130ff8a9e6e36b23aa52677c
gururaj02 <ngururaj58@gmail.com>
2026-05-03 23:31:33 +0530

    Update AccountServiceImplTest.java

--- src/test/java/com/example/banking_application_spring_boot/service/impl/AccountServiceImplTest.java
+++ src/test/java/com/example/banking_application_spring_boot/service/impl/AccountServiceImplTest.java
@@ -188,3 +188,7 @@ public class AccountServiceImplTest {
         });
     }
 }
+
+// TODO: write more test cases
+// TODO: Write Loggers
+// TODO: Add Simple events - like sending email etc., (for learning)
