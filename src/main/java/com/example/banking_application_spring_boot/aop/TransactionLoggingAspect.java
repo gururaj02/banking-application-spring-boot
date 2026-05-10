fc8af3cbbc4a657356a363a9569937196704805f
gururaj02 <ngururaj58@gmail.com>
2026-05-10 11:15:26 +0530

    Create TransactionLoggingAspect.java

---
+++ src/main/java/com/example/banking_application_spring_boot/aop/TransactionLoggingAspect.java
@@ -0,0 +1,26 @@
+package com.example.banking_application_spring_boot.aop;
+
+import com.example.banking_application_spring_boot.dto.AccountDto;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class TransactionLoggingAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransactionLoggingAspect.class);
+
+    @AfterReturning(pointcut = "execution(* com.example.banking_application_spring_boot.service.impl.AccountServiceImpl.deposit(..))", returning = "result")
+    public void logDepositSuccess(JoinPoint joinPoint, Object result) {
+        Object[] args = joinPoint.getArgs();
+        double amount = (double) args[0];
+
+        AccountDto accountDto = (AccountDto) result;
+
+        LOGGER.info("Deposit successful: Account Number = {}, Amount = {}", accountDto.id(), amount);
+    }
+}
