79f1960e3b7e2abafe140b93e3ab7177429f3c5c
gururaj02 <ngururaj58@gmail.com>
2026-05-12 22:37:21 +0530

    removed permission for actuator endpoint

--- src/main/java/com/example/banking_application_spring_boot/config/SecurityConfig.java
+++ src/main/java/com/example/banking_application_spring_boot/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/authenticate").permitAll()
-                                .requestMatchers("/api/users/register").permitAll()
+                                .requestMatchers("/api/users/register", "/actuator/**").permitAll()
                                 .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
