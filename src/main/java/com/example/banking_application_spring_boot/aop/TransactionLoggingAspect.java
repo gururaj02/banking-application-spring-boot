ef6718ce65a21b71a2a0e0ea08cc32e12b8be3d7
gururaj02 <ngururaj58@gmail.com>
2026-05-12 22:29:46 +0530

    added more loggers for withdraw and transfer

--- src/main/java/com/example/banking_application_spring_boot/aop/TransactionLoggingAspect.java
+++ src/main/java/com/example/banking_application_spring_boot/aop/TransactionLoggingAspect.java
@@ -30,7 +30,32 @@ public class TransactionLoggingAspect {
         LOGGER.error("Deposit failed: Amount = {}, Error = {}", amount, exception.getMessage());
     }
 
+    @AfterReturning(pointcut = "execution(* com.example.banking_application_spring_boot.service.impl.AccountServiceImpl.withdraw(..))", returning = "result")
+    public void logWithdrawSuccess(JoinPoint joinPoint, Object result) {
+        Object[] args = joinPoint.getArgs();
+        double amount = (double) args[0];
+
+        AccountDto accountDto = (AccountDto) result;
+
+        LOGGER.info("Withdraw successful: Account Number = {}, Amount = {}", accountDto.id(), amount);
+    }
+
+    @AfterThrowing(pointcut = "execution(* com.example.banking_application_spring_boot.service.impl.AccountServiceImpl.withdraw(..)) && args(amount, securityPin)", throwing = "exception")
+    public void logWithdrawFailure(double amount, String securityPin, Exception exception) {
+        LOGGER.error("Withdraw failed: Amount = {}, Error = {}", amount, exception.getMessage());
+    }
+
+    @AfterReturning(pointcut = "execution(* com.example.banking_application_spring_boot.service.impl.AccountServiceImpl.transfer(..))", returning = "result")
+    public void logTransferSuccess(JoinPoint joinPoint, Object result) {
+        Object[] args = joinPoint.getArgs();
+        String receiverAccountNumber = (String) args[0];
+        double amount = (double) args[1];
+        AccountDto accountDto = (AccountDto) result;
+        LOGGER.info("Transfer successful: From Account = {}, To Account = {}, Amount = {}", accountDto.id(), receiverAccountNumber, amount);
+    }
+
+    @AfterThrowing(pointcut = "execution(* com.example.banking_application_spring_boot.service.impl.AccountServiceImpl.transfer(..)) && args(receiverAccountNumber, amount, securityPin)", throwing = "exception")
+    public void logTransferFailure(String receiverAccountNumber, double amount, String securityPin, Exception exception) {
+        LOGGER.error("Transfer failed: To Account = {}, Amount = {}, Error = {}", receiverAccountNumber, amount, exception.getMessage());
+    }
 }
-
-
-// TODO: Add more loggers for withdraw and transfer methods
