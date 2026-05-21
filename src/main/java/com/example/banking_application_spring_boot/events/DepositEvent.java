44aec7bf8f7313e78e33bc0a01c2246e1f97f4a2
gururaj02 <ngururaj58@gmail.com>
2026-05-21 22:50:32 +0530

    Update DepositEvent.java

--- src/main/java/com/example/banking_application_spring_boot/events/DepositEvent.java
+++ src/main/java/com/example/banking_application_spring_boot/events/DepositEvent.java
@@ -1,4 +1,20 @@
 package com.example.banking_application_spring_boot.events;
 
+
 public class DepositEvent {
+    private String accountNumber;
+    private double amount;
+
+    public DepositEvent(String accountNumber, double amount) {
+        this.accountNumber = accountNumber;
+        this.amount = amount;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
 }
