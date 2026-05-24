63f309c947977653075f946acc4a71b67a877800
gururaj02 <ngururaj58@gmail.com>
2026-05-24 22:15:24 +0530

    Conditional events added for high value deposit

---
+++ src/main/java/com/example/banking_application_spring_boot/listeners/HighAmountDepositListener.java
@@ -0,0 +1,18 @@
+package com.example.banking_application_spring_boot.listeners;
+
+
+import com.example.banking_application_spring_boot.events.DepositEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+// this is the conditional listener
+
+@Component
+public class HighAmountDepositListener {
+
+    @EventListener(condition = "#event.amount > 50000")
+    public void handleHighAmountDeposit(DepositEvent event) {
+        // Logic to handle high amount deposit, e.g., send notification, log the event, etc.
+        System.out.println("High amount deposit detected for account " + event.getAccountNumber() + ": $" + event.getAmount());
+    }
+}
